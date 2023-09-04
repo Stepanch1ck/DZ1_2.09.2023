9b895ab4ce832502ef0598f27a60649dbf85024c
nonam <nonam@NOTEBOOKE>
2023-09-04 19:37:28 +0300

    Добавьте файлы проекта.

---
+++ DZ1_2.09.2023/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+class Progrem
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Напишите программу, выводит на экран три строки \"Мир\", \"Труд\" и \"Май\" (кавычки\r\nне нужны).")
+        Console.WriteLine("Мир Труд Май");
+        Console.WriteLine("Мир");
+        Console.WriteLine("   Труд");
+        Console.WriteLine("       Май");
+        Console.ReadKey();
+
+
+    }
+}
