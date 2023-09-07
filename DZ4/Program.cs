8accd48bc9e107b38bf83d1d2d86e0094c5ae437
Stepan Ch <noname.churkin@gmail.com>
2023-09-07 20:14:21 +0300

    р

--- DZ4/Program.cs
+++ DZ4/Program.cs
@@ -8,14 +8,17 @@ class Progrem
     {
         Console.WriteLine("4 задание");
         Console.WriteLine("Найти значение y=cos(x)");
+
         Console.WriteLine("Введите y в градусах:");
         string input1 = Console.ReadLine();
         double y1 = Convert.ToDouble(input1);
         Console.WriteLine(Math.Cos(y1 * Math.PI / 180));
         Console.WriteLine("Введите y в радианах(дробное число без Pi):");
+
         string input2 = Console.ReadLine();
         double y2 = Convert.ToDouble(input2);
         Console.WriteLine(Math.Cos(y2));
+
         Console.WriteLine("Нажмите enter");
         Console.ReadKey();
 
