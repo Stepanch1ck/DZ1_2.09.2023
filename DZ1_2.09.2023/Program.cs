81f20a93bc99cd8bbbf93310d6c967ec819d30eb
Stepan Ch <noname.churkin@gmail.com>
2023-09-07 15:57:38 +0300

    k

--- DZ1_2.09.2023/Program.cs
+++ DZ1_2.09.2023/Program.cs
@@ -1,13 +1,18 @@
 ﻿using System;
+
+namespace домашнее_задание_1;
+
 class Progrem
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Напишите программу, выводит на экран три строки \"Мир\", \"Труд\" и \"Май\" (кавычки\r\nне нужны).")
+        Console.WriteLine("1 задание");
+        Console.WriteLine("Напишите программу, выводит на экран три строки \"Мир\", \"Труд\" и \"Май\" (кавычки\r\nне нужны).");
         Console.WriteLine("Мир Труд Май");
         Console.WriteLine("Мир");
         Console.WriteLine("   Труд");
         Console.WriteLine("       Май");
+        Console.WriteLine("Нажмите, чтобы закрыть");
         Console.ReadKey();
 
 
---
+++ DZ2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace домашнее_задание_2;
+class Progrem
+{
+    static void Main(string[] args)
+    {
+        
+        Console.WriteLine("Задание 2 Программа просит пользователя ввести 2 числовые переменные. А после она меняет их местами и выводит результат на экран.");
+
+        Console.WriteLine("Введите первое число");
+        string chis1 = Console.ReadLine();
+        chis1 = chis1.Replace(',', '.');
+        if (!int.TryParse(chis1, out int chis1_int) || !double.TryParse(chis1, out double chis1_doub)) {    
+        } else {
+            Console.WriteLine("Вы ввели не число");
+        }
+
+
+        Console.WriteLine("Введите второе число");
+        string chis2 = Console.ReadLine();
+        chis2 = chis2.Replace(',', '.');
+        if (!int.TryParse(chis2, out int chis2_int) || !double.TryParse(chis1, out double chis2_doub)) {
+        } else {
+            Console.WriteLine("Вы ввели не число");
+        }
+        Console.WriteLine("Результат:" + chis2 + " " + chis1);
+
+
+
+
+    }
+}
